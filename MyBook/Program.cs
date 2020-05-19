466ccc74f55fd1aa12b73141c113bfa90db505a4
s2018 <s2018s35@e-mirim.hs.kr>
2020-05-19 09:49:36 +0900

    초기 커밋.

---
+++ MyBook/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBook
+{
+    class Program
+    {
+        class Book
+        {
+            Book book = new Book();
+
+            public string bookname = "PHP 프로그래밍 입문";
+            public string firstprint = "2013년 5월 20일";
+            public string writeperson = "황재호";
+            public string person = "김태헌";
+            public string where = "한빛아카데미(주)";
+            public string editperson = "김현용";
+            public string think = "김이화";
+            public string edit = "김이화";
+            public string design = "여동일";
+        }
+
+        static void Main(string[] args)
+        {
+          
+       
+        }
+    }
+}
